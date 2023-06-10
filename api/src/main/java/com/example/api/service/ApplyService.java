6f88fbc6e98a642c53e3abc34468dab365a8fc42
Yunsik-Choi <choiys0311@gmail.com>
2023-06-10 17:23:46 +0900

    한 명당 하나의 쿠폰만 발급 및 이벤트 실패 로직 수정

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,24 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
     public ApplyService(final CouponRepository couponRepository, final CouponCountRepository couponCountRepository,
-                        final CouponCreateProducer couponCreateProducer) {
+                        final CouponCreateProducer couponCreateProducer,
+                        final AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if(apply != 1) {
+            return;
+        }
+
         long count = couponCountRepository.increment();
 
         if (count > 100) {
--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -51,4 +51,28 @@ class ApplyServiceTest {
 
         assertThat(couponRepository.count()).isEqualTo(100);
     }
+
+    @Test
+    void 한명당_한개의쿠폰만_발급() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        Thread.sleep(10000);
+
+        assertThat(couponRepository.count()).isEqualTo(1);
+    }
 }
